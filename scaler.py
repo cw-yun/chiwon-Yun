6d1730731c5c7297a4f991bf743bf332f22411ad
eyenix49 <88366877+eyenix49@users.noreply.github.com>
2021-09-14 17:02:18 +0900

    Add files via upload

--- scaler.py
+++ scaler.py
@@ -1,124 +1,106 @@
 from PIL import Image
 import numpy as np
 
-def nearest(im,coordinate):
-    pixel = im.load()
-    if (coordinate[0] < 0):
-        coordinate = (0, coordinate[1])
-    if (coordinate[1] < 0):
-        coordinate = (coordinate[0], 0)
-    if (coordinate[0] > im.size[0] - 2):
-        coordinate = (im.size[0] - 2, coordinate[1])
-    if (coordinate[1] > im.size[1] - 2):
-        coordinate = (coordinate[0], im.size[1] - 2)
+def nearest(input_size, output_size):
+    original_img = Image.open("my_image.JPG")
+    pixel = original_img.load()
 
-    #if (coordinate[0] == int(coordinate[0]) and coordinate[1] == int(coordinate[1])):
-    #    return pixel(coordinate)
+    width_scale = output_size[0] / input_size[0]
+    height_scale = output_size[1] / input_size[1]
 
-    left = int(coordinate[0])
-    right = int(coordinate[0]) + 1
-    top = int(coordinate[1])
-    bottom = int(coordinate[1]) + 1
-
-    a = coordinate[0] - int(coordinate[0])
-    b = coordinate[1] - int(coordinate[1])
-    #print(a)
-    #print(b)
-    A = pixel[left, top]
-    B = pixel[right, top]
-    C = pixel[left, bottom]
-    D = pixel[right, bottom]
-
-    if a <= 0.5 and b <= 0.5:
-        X = (A[0],A[1],A[2])
-        #print("a")
-    elif a > 0.5 and b <= 0.5:
-        X = (B[1],B[2],B[3])
-        #print("b")
-    elif a <= 0.5 and b > 0.5:
-        X = (C[0],C[1],C[2])
-        #print("c")
-    elif a > 0.5 and b > 0.5:
-        X = (D[0],D[1],D[2])
-        #print("d")
-    return X
-
-
-def bilinear(im,coordinate,magnitude):
-    #print(coordinate[0])
-    #print(coordinate[1])
-    pixel = im.load()
-    coordinate[0] = coordinate[0] * magnitude
-    coordinate[1] = coordinate[1] * magnitude
-    #print(coordinate[0])
-    #print(coordinate[1])
-    if(coordinate[0]<0):
-        coordinate = (0,coordinate[1])
-    if(coordinate[1]<0):
-        coordinate = (coordinate[0],0)
-    if(coordinate[0]>im.size[0]-2):
-        coordinate = (im.size[0]-2, coordinate[1])
-    if(coordinate[1]>im.size[1]-2):
-        coordinate = (coordinate[0], im.size[1]-2)
-
-    #if(coordinate[0]==int(coordinate[0]) and coordinate[1]==int(coordinate[1])):
-    #    return pixel(coordinate)
-
-    left = int(coordinate[0])
-    right = int(coordinate[0]) + 1
-    top = int(coordinate[1])
-    bottom = int(coordinate[1]) + 1
-
-    a = coordinate[0] - int(coordinate[0])
-    b = coordinate[1] - int(coordinate[1])
-
-    A = pixel[left,top]
-    B = pixel[right,top]
-    C = pixel[left,bottom]
-    D = pixel[right,bottom]
-    E = ((1-a)*A[0]+a*B[0],(1-a)*A[1]+a*B[1],(1-a)*A[2]+a*B[2])
-    F = ((1-a)*C[0]+a*D[0],(1-a)*C[1]+a*D[1],(1-a)*C[2]+a*D[2])
-    X = (int((1-b)*E[0]+b*F[0]),int((1-b)*E[1]+b*F[1]),int((1-b)*E[2]+b*F[2]))
-    return X
-
-def Magnify_nearest(im, magnitude):
-    modified_size = (int(im.size[0] * magnitude), int(im.size[1] * magnitude))
-    modified_rgb = Image.new("RGB", modified_size)
-    modified_pixel = modified_rgb.load()
-    for i in range(modified_size[0]):
-        for j in range(modified_size[1]):
-            coordinate = (int(i / magnitude), int(j / magnitude))
-            modified_pixel[i,j] = nearest(im, coordinate)
-
-    print(modified_rgb)
-    return modified_rgb
-
-
-def Magnify_bilinear(im, magnitude):
-    modified_size = (int(im.size[0] * magnitude), int(im.size[1] * magnitude))
-    modified_rgb = Image.new("RGB", modified_size)
-    modified_pixel = modified_rgb.load()
-
+    rgb = {}
     sum_rgb = []
-    for i in range(modified_size[0]):
-        for j in range(modified_size[1]):
-            coordinate = [int(i/magnitude),int(j/magnitude)]
-            modified_pixel[i,j] = bilinear(im,coordinate,magnitude)
-            sum_rgb += modified_pixel[i,j]
-            #print(sum_rgb)
+    for j in range(output_size[1]):
+        for i in range(output_size[0]):
+            position = [i/width_scale, j/height_scale]
+            if (position[0] < 0):
+                position = (0, position[1])
+            if (position[1] < 0):
+                position = (position[0], 0)
+            if (position[0] > original_img.size[0] - 2):
+                position = (original_img.size[0] - 2, position[1])
+            if (position[1] > original_img.size[1] - 2):
+                position = (position[0], original_img.size[1] - 2)
+
+            left = int(position[0])
+            right = int(position[0]) + 1
+            top = int(position[1])
+            bottom = int(position[1]) + 1
+
+            a = position[0] - int(position[0])
+            b = position[1] - int(position[1])
+
+            A = pixel[left, top]
+            B = pixel[right, top]
+            C = pixel[left, bottom]
+            D = pixel[right, bottom]
+
+            if a <= 0.5 and b <= 0.5:
+                X = (A[0], A[1], A[2])
+            elif a > 0.5 and b <= 0.5:
+                X = (B[0], B[1], B[2])
+            elif a <= 0.5 and b > 0.5:
+                X = (C[0], C[1], C[2])
+            elif a > 0.5 and b > 0.5:
+                X = (D[0], D[1], D[2])
+
+            rgb[i,j] = X
+            sum_rgb += rgb[i,j]
     sum_rgb = np.array(sum_rgb)
-    #print(sum_rgb)
-    result = np.reshape(sum_rgb,(modified_size[0],modified_size[1],3))
-    #result = np.array(sum_rgb.reshpae(modified_size[0],modified_size[1],3))
-    #print(result)
-    image_rgb = Image.fromarray(sum_rgb)
-    print(image_rgb)
-    image_rgb = image_rgb.convert("RGB")
+    reshape_rgb = np.reshape(sum_rgb, (output_size[1], output_size[0], 3))
+    image_rgb = Image.fromarray(reshape_rgb.astype('uint8'))
     print(image_rgb)
     return image_rgb
 
-im = Image.open("my_image.JPG")
-im_nearest = Magnify_nearest(im,1.5)
-im_nearest.save("nearest.JPG")
-im_bilinear = Magnify_bilinear(im,1.5)
-im_bilinear.save("bilinear.JPG")
+
+def bilinear(input_size,output_size):
+    original_img = Image.open("my_image.JPG")
+    pixel = original_img.load()
+
+    width_scale = output_size[0] / input_size[0]
+    height_scale = output_size[1] / input_size[1]
+
+    rgb = {}
+    sum_rgb = []
+    for j in range(output_size[1]):
+        for i in range(output_size[0]):
+            position = [i/width_scale, j/height_scale]
+            if (position[0] < 0):
+                position = (0, position[1])
+            if (position[1] < 0):
+                position = (position[0], 0)
+            if (position[0] > original_img.size[0] - 2):
+                position = (original_img.size[0] - 2, position[1])
+            if (position[1] > original_img.size[1] - 2):
+                position = (position[0], original_img.size[1] - 2)
+
+            left = int(position[0])
+            right = int(position[0]) + 1
+            top = int(position[1])
+            bottom = int(position[1]) + 1
+
+            a = position[0] - int(position[0])
+            b = position[1] - int(position[1])
+
+            A = pixel[left, top]
+            B = pixel[right, top]
+            C = pixel[left, bottom]
+            D = pixel[right, bottom]
+            E = ((1 - a) * A[0] + a * B[0], (1 - a) * A[1] + a * B[1], (1 - a) * A[2] + a * B[2])
+            F = ((1 - a) * C[0] + a * D[0], (1 - a) * C[1] + a * D[1], (1 - a) * C[2] + a * D[2])
+            X = (int((1 - b) * E[0] + b * F[0]), int((1 - b) * E[1] + b * F[1]), int((1 - b) * E[2] + b * F[2]))
+            rgb[i, j] = X
+            sum_rgb += rgb[i, j]
+    sum_rgb = np.array(sum_rgb)
+    reshape_rgb = np.reshape(sum_rgb, (output_size[1], output_size[0], 3))
+    image_rgb = Image.fromarray(reshape_rgb.astype('uint8'))
+    print(image_rgb)
+    return image_rgb
+
+original_img = Image.open("my_image.JPG")
+original_size = original_img.size
+output_size = (456,638)                # set output_size
+nearest_img = nearest(original_size,output_size)
+nearest_img.save("nearest.JPG")
+bilinear_img = bilinear(original_size,output_size)
+bilinear_img.save("bilinear.JPG")

3fc08ff6f3a731f79d57508f005d62d1ea84c8fe
eyenix49 <88366877+eyenix49@users.noreply.github.com>
2021-09-17 16:42:52 +0900

    Add files via upload

--- mnist.py
+++ mnist.py
@@ -13,7 +13,9 @@ def Convolution(input,kernel):
                 for l in range(new_height):
                     conv.append((input[i,k:k + kernel_width, l:l + kernel_height] * kernel[j]).sum())
 
-    conv = np.array(conv).reshape(num_channel,new_width,new_height)
+    global num_all_channel
+    num_all_channel *= num_channel
+    conv = np.array(conv).reshape(num_all_channel,new_width,new_height)
     print(conv.shape)
     return conv
 
@@ -40,7 +42,10 @@ def Max_pooling(input,kernel_size):
     print(pool.shape)
     return pool
 
-#def Fully_connected:
+def Fully_connected(input):
+    num_input, input_width, input_height = input.shape
+    flatten = input.reshape(num_input * input_width * input_height)
+    print(flatten.shape)
 
 
 test_data = np.loadtxt('test.csv', delimiter=',', dtype=np.float32)
@@ -48,18 +53,26 @@ test_data = np.loadtxt('test.csv', delimiter=',', dtype=np.float32)
 reshape_test_data = test_data[1:].reshape(1,28,28)
 #print(reshape_test_data.shape)
 
-Conv1_kernel = [[[0.01,0,0.01,0,0.01],[0,0.01,0,0.01,0],[0.01,0,0.01,0,0.01],[0,0.01,0,0.01,0],[0.01,0,0.01,0,0.01]],              # 6 channels, 5*5 kernel
+num_all_channel = 1
+
+Conv1_kernel = [[[0.01,0,0.01,0,0.01],[0,0.01,0,0.01,0],[0.01,0,0.01,0,0.01],[0,0.01,0,0.01,0],[0.01,0,0.01,0,0.01]],            # 6 channels, 5*5 kernel
                 [[0.011,0,0.011,0,0.011],[0,0.011,0,0.011,0],[0.011,0,0.011,0,0.011],[0,0.011,0,0.011,0],[0.011,0,0.011,0,0.011]],
                 [[0.012,0,0.012,0,0.012],[0,0.012,0,0.012,0],[0.012,0,0.012,0,0.012],[0,0.012,0,0.012,0],[0.012,0,0.012,0,0.012]],
                 [[0.013,0,0.013,0,0.013],[0,0.013,0,0.013,0],[0.013,0,0.013,0,0.013],[0,0.013,0,0.013,0],[0.013,0,0.013,0,0.013]],
                 [[0.014,0,0.014,0,0.014],[0,0.014,0,0.014,0],[0.014,0,0.014,0,0.014],[0,0.014,0,0.014,0],[0.014,0,0.014,0,0.014]],
                 [[0.015,0,0.015,0,0.015],[0,0.015,0,0.015,0],[0.015,0,0.015,0,0.015],[0,0.015,0,0.015,0],[0.015,0,0.015,0,0.015]]]
 Conv1_kernel = np.array(Conv1_kernel)
-#print(Conv1_kernel.shape)
 Pool1_kernel_channel_size = (2,2)                                                                                                # 2*2 down sampling
+
+Conv2_kernel = [[[0.01,0,0.01,0,0.01],[0,0.01,0,0.01,0],[0.01,0,0.01,0,0.01],[0,0.01,0,0.01,0],[0.01,0,0.01,0,0.01]],            # 3 channels, 5*5 kernel
+                [[0.011,0,0.011,0,0.011],[0,0.011,0,0.011,0],[0.011,0,0.011,0,0.011],[0,0.011,0,0.011,0],[0.011,0,0.011,0,0.011]],
+                [[0.012,0,0.012,0,0.012],[0,0.012,0,0.012,0],[0.012,0,0.012,0,0.012],[0,0.012,0,0.012,0],[0.012,0,0.012,0,0.012]]]
+Conv2_kernel = np.array(Conv2_kernel)
+Pool2_kernel_channel_size = (2,2)                                                                                                # 2*2 down sampling
 
 Conv1 = Convolution(reshape_test_data,Conv1_kernel)
 Max_pooling1 = Max_pooling(Conv1,Pool1_kernel_channel_size)
 
-Conv2 = Convolution(Max_pooling1,Conv1_kernel)
-Max_pooling2= Max_pooling(Conv2,Pool1_kernel_channel_size)
+Conv2 = Convolution(Max_pooling1,Conv2_kernel)
+Max_pooling2= Max_pooling(Conv2,Pool2_kernel_channel_size)
+Fully_connected = Fully_connected(Max_pooling2)
